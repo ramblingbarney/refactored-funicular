aad0c98235e02ca58ecb0bee9477d04caab47a08
Conor O'Dwyer <c9dw5er@protonmail.com>
2018-07-04 14:44:20 +0100

    - config.py 'DevelopmentConfig' & 'TestingConfig' configured correctly

--- app.py
+++ app.py
@@ -4,67 +4,79 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
-app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 
-# app.config["MONGO_DBNAME"] = config.DevelopmentConfig.MONGO_DBNAME
-# app.config["MONGO_URI"] = config.DevelopmentConfig.MONGO_URI
+def create_app():
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.from_object('config.ProductionConfig')
+        app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 
-mongo = PyMongo(app)
+    elif os.getenv('FLASK_CONFIG') == "development":
+        app = Flask(__name__)
+        app.config.from_object('config.DevelopmentConfig')
+        app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 
-
-@app.route('/new_category')
-def new_category():
-    return render_template('add_category.html')
-
-
-@app.route('/insert_category', methods=['POST'])
-def insert_category():
-    categories = mongo.db.categories
-    category_doc = {'category_name': request.form['category_name']}
-    categories.insert_one(category_doc)
-    return redirect(url_for('get_categories'))
+    else:
+        app = Flask(__name__)
+        app.config.from_object('config.TestingConfig')
+        app.config.from_envvar('YOURAPPLICATION_SETTINGS')
 
 
-@app.route('/get_categories')
-def get_categories():
-    return render_template('categories.html',
-                            categories=mongo.db.categories.find())
+    mongo = PyMongo(app)
 
 
-@app.route('/edit_category/<category_id>')
-def edit_category(category_id):
-    return render_template('edit_category.html',
-        category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+    @app.route('/new_category')
+    def new_category():
+        return render_template('add_category.html')
 
 
-@app.route('/category_item/<category_id>', methods=["POST"])
-def update_category(category_id):
-    category_items = mongo.db.categories
-    category_items.update( {'_id': ObjectId(category_id)},
-                            { 'category_name': request.form['category_name'] })
-    return redirect(url_for('get_categories'))
+    @app.route('/insert_category', methods=['POST'])
+    def insert_category():
+        categories = mongo.db.categories
+        category_doc = {'category_name': request.form['category_name']}
+        categories.insert_one(category_doc)
+        return redirect(url_for('get_categories'))
 
 
-@app.route('/delete_category/<category_id>')
-def delete_category(category_id):
-    mongo.db.categories.remove({'_id': ObjectId(category_id)})
-    return redirect(url_for("get_categories"))
+    @app.route('/get_categories')
+    def get_categories():
+        return render_template('categories.html',
+                                categories=mongo.db.categories.find())
 
 
-@app.route('/add_recipe')
-def add_recipe():
-    return render_template('add_recipe.html',
-    categories=mongo.db.categories.find())
+    @app.route('/edit_category/<category_id>')
+    def edit_category(category_id):
+        return render_template('edit_category.html',
+            category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
 
 
-@app.route('/insert_recipe', methods=['POST'])
-def insert_recipe():
-    recipes =  mongo.db.recipes
-    recipies.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipes'))
+    @app.route('/category_item/<category_id>', methods=["POST"])
+    def update_category(category_id):
+        category_items = mongo.db.categories
+        category_items.update( {'_id': ObjectId(category_id)},
+                                { 'category_name': request.form['category_name'] })
+        return redirect(url_for('get_categories'))
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            debug=True)
+
+    @app.route('/delete_category/<category_id>')
+    def delete_category(category_id):
+        mongo.db.categories.remove({'_id': ObjectId(category_id)})
+        return redirect(url_for("get_categories"))
+
+
+    @app.route('/add_recipe')
+    def add_recipe():
+        return render_template('add_recipe.html',
+        categories=mongo.db.categories.find())
+
+
+    @app.route('/insert_recipe', methods=['POST'])
+    def insert_recipe():
+        recipes =  mongo.db.recipes
+        recipies.insert_one(request.form.to_dict())
+        return redirect(url_for('get_recipes'))
+
+    if __name__ == '__main__':
+        app.run(host=os.environ.get('IP'),
+                debug=True)
+create_app()
--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 class Config(object):
-    DEBUG = False
-    TESTING = False
+    DEBUG = True
 
 
 class ProductionConfig(Config):
     True
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
     MONGO_DBNAME = 'recipe_buddy'
     MONGO_URI = 'mongodb://recipeapp:Changethis$3d@ds261570.mlab.com:61570/recipe_buddy'
 
+
 class TestingConfig(Config):
     TESTING = True
-    MONGO_DBNAME = 'recipe_buddy'
-    MONGO_URI = 'mongodb://recipeapp:Changethis$3d@ds261570.mlab.com:61570/recipe_buddy'
+    DEBUG = True
+    MONGO_DBNAME = 'recipe_app_testing'
+    MONGO_URI = 'mongodb://recipeapptester:changethis5@ds125331.mlab.com:25331/recipe_app_testing'
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig
+}
