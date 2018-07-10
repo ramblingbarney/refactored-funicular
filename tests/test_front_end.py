abae85ad49cb28f87d7b4266ffa476f7ab419e58
Conor O'Dwyer <c9dw5er@protonmail.com>
2018-07-10 14:27:06 +0100

    This piece of work is to redo unit tests for the specific purpose of using selenium webdriver only and creating non overlapping descrete unit tests based on mongodb fixtures
    - Removed beautifulsoup module and unit tests utilising this module
    - Removed datetime, time module & use in unit tests
    - Created test for 'test_three_categories' fixtures using selenium webdriver only

--- tests/test_front_end.py
+++ tests/test_front_end.py
@@ -13,9 +13,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import helper_Web_Driver_Wait_CSS_Element
-from bs4 import BeautifulSoup
-import time
-import datetime
 
 
 class RecipeBuddyUITests(unittest.TestCase):
@@ -40,6 +37,8 @@ class RecipeBuddyUITests(unittest.TestCase):
         # create the 'categories' collection in MongoDB
         self.collection_categories = self.DB.categories
 
+        # categores to insert
+
         category_1 = {
             'category_name': 'Thai'
         }
@@ -50,17 +49,14 @@ class RecipeBuddyUITests(unittest.TestCase):
             'category_name': 'Indian'
         }
 
-        # create categories collection
+        # insert categories collection
         new_result = self.collection_categories.insert_many([category_1,
                                                         category_2,
                                                         category_3])
 
-        print('Multiple posts: {0}'.format(new_result.inserted_ids))
-
         # creates a test client
         self.app = app.test_client()
 
-        self.app = app.test_client()
         # propagate the exceptions to the test client
         self.app.testing = True
 
@@ -69,7 +65,6 @@ class RecipeBuddyUITests(unittest.TestCase):
         options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=options)
 
-        self.today = datetime.date.today()
         self.elements = []
 
     def tearDown(self):
@@ -79,86 +74,16 @@ class RecipeBuddyUITests(unittest.TestCase):
 
     def test_three_categories(self):
         ''' Test 3 categories present '''
-        self.assertEqual(1, 1)
 
-        
-    def test_add_category(self):
-        ''' Test Adding a Category'''
-        self.assertEqual(1, 1)
+        page = self.driver.get("http://localhost:5000/get_categories")
+        self.driver.implicitly_wait(0) # seconds
 
-    #
-    # def test_add_category(self):
-    #     ''' Test Adding a Category'''
-    #     page = self.driver.get("http://localhost:5000/get_categories")
-    #     self.driver.implicitly_wait(0) # seconds
-    #     element = self.driver.find_element_by_id("add_category")
-    #     element.click()
-    #     self.driver.implicitly_wait(0) # seconds
-    #     self.driver.find_element_by_id("category_name").send_keys(
-    #                                         'PPPPPPPPPPPP' + str(self.today))
-    #     self.driver.implicitly_wait(0) # seconds
-    #     added_category_button = self.driver.find_element_by_id("add_category")
-    #     added_category_button.click()
-    #     time.sleep(3)
-    #     soup = BeautifulSoup(self.driver.page_source, 'html5lib')
-    #     for line in soup.find('div', {'class': 'category_container'}).find_all(
-    #             'span', {'class': 'category_list_item'}):
-    #         self.elements.append(line.text.strip())
-    #     self.assertIn('PPPPPPPPPPPP' + str(self.today), self.elements)
-#
-#     def test_delete_a_category(self):
-#         ''' Test Deleting a Categories item'''
-#         page = self.driver.get("http://localhost:5000/get_categories")
-#         time.sleep(3)
-#         delete_buttons = self.driver.find_elements_by_class_name(
-#             "delete_category_button")
-#         delete_buttons_before_count = len(delete_buttons)
-#         for x in delete_buttons:
-#             try:
-#                 x.click()
-#                 time.sleep(3)
-#             except: # take this out helper delete first cat, combine lines 55 and 60 not a loop, delte ct not exit rais excpetion, tyr to avide bs4 or if I have to explain why in comments
-#                 continue
-#         time.sleep(3)
-#         soup = BeautifulSoup(self.driver.page_source, 'html5lib')
-#         for line in soup.find('div', {'class': 'category_container'}).find_all(
-#                 'span', {'class': 'category_list_item'}):
-#             self.elements.append(line.text.strip())
-#         self.assertEqual(delete_buttons_before_count, len(self.elements) + 1)
-# #delte last category
-# # first test to create a clearn state, I know x and y exists
-#     def test_edit_category(self):
-#         ''' Test editing a Category'''
-#         page = self.driver.get("http://localhost:5000/get_categories")
-#         time.sleep(3)
-#         element = self.driver.find_elements_by_class_name("edit_category_button")
-#         # time.sleep(3)
-#         element[0].click()
-#         time.sleep(3)
-#         self.driver.find_element_by_id("category_name").send_keys(
-#                                             'EditCategoryTest' + str(self.today))
-#         # time.sleep(3)
-#         edit_category_button = self.driver.find_element_by_id("edit_category")
-#         edit_category_button.click()
-#         time.sleep(3) #find_elements_by_css_selecto
-#         soup = BeautifulSoup(self.driver.page_source, 'html5lib')
-#         for line in soup.find('div', {'class': 'category_container'}).find_all(
-#                 'span', {'class': 'category_list_item'}):
-#             self.elements.append(line.text.strip())
-#         # one_element_sting = ''.join(self.elements)
-#         # self.assertIn('EditCategoryTest' + str(self.today), one_element_sting)
-#         self.assertIn('EditCategoryTest' + str(self.today), self.elements)
-#
-#     def test_cancel_edit_category(self):
-#         ''' Test cancelling editing a Category'''
-#         page = self.driver.get("http://localhost:5000/get_categories")
-#         time.sleep(3)
-#         element = self.driver.find_elements_by_class_name(
-#             "edit_category_button")
-#         time.sleep(3)
-#         element[0].click()
-#         time.sleep(3)
-#         self.driver.find_element_by_id("cancel_category").click()
-#         time.sleep(3)
-#         self.assertEqual(self.driver.current_url,
-#                     'http://localhost:5000/get_categories')
+        self.elements = self.driver.find_elements_by_class_name("category_list_item")
+
+        li_span_text = []
+        test_list = ['Thai','Chinese','Indian']
+
+        for element in self.elements:
+            li_span_text.append(element.text)
+
+        self.assertListEqual(test_list, li_span_text)
