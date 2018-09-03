57cbf97e179934aef8fb837306854e358706b4d5
Conor O'Dwyer <c9dw5er@protonmail.com>
2018-09-03 22:50:02 +0100

    - Updated style.css for 'register.html' styling changes - Added classes for labels and headings to 'register.html' template

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
-
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
