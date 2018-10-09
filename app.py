c3310969c50f525c3ca96866df6009f7491175f3
Conor O'Dwyer <c9dw5er@protonmail.com>
2018-10-09 23:13:47 +0100

    - Amended home route to use os.environ['CURRENT_HOST'] and not localhost - Pep8 guideline changes

--- app.py
+++ app.py
@@ -233,18 +233,22 @@ def home():
 
     for x in chart_data:
         result_cleaned.append({
-            'name': x['_id']['cuisine_name'], 'value': x['total_votes'],
-            'url': 'http://localhost:5000/chart_search_recipes/'
-            + x['_id']['cuisine_name']})
+            'name': x['_id']['cuisine_name'],
+            'value': x['total_votes'],
+            'url': os.environ['CURRENT_HOST'] +
+            '/chart_search_recipes/' + x['_id']['cuisine_name']})
 
     if (len(result_cleaned) == 0):
         result_cleaned.append({
             'name': 'No Recipes, register to add recipes',
-            'value': 1, 'url': 'http://localhost:5000/register'})
+            'value': 1, 'url': os.environ['CURRENT_HOST'] + '/register'})
 
     chart_data = dumps({'name': 'A1', 'children': result_cleaned}, indent=2)
+
     return render_template(
-        'index.html', resultData=chart_data, chart_switch=len(result_cleaned))
+        'index.html',
+        resultData=chart_data,
+        chart_switch=len(result_cleaned))
 
 
 # somewhere to login
